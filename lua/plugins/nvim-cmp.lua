cace8d9134c27058e44e813df6152562f0a9cc39
Pavel Kuldyshev <pk@fabuza.ru>
2023-10-11 03:23:38 +0300

    Was updated mapping for cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -13,18 +13,26 @@ cmp.setup({
         end,
     },
     mapping = {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          local entry = cmp.get_selected_entry()
-          if not entry then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+       ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
           else
-            cmp.confirm()
+              fallback()
           end
-        else
-          fallback()
-        end
       end, { "i", "s", "c" }),
+      
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+          else
+              fallback()
+          end
+      end, { "i", "s", "c" }),
+
+      ["<CR>"] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true,
+      }),
     },
     sources = {
         { name = 'nvim_lsp' },
